b947fe39b3a685b36470333b510ac8683c2c652a
Below Bed <jordanmeta4677@gmail.com>
2022-05-09 21:19:55 +0800

    search members

--- src/routes/app/jadwal/[id]/member.js
+++ src/routes/app/jadwal/[id]/member.js
@@ -1,10 +1,19 @@
 import { db } from '../../../../db'
 
 export async function get(event) {
+  const jadwalId = parseInt(event.params.id)
+  let where = { jadwalId }
+  let keyword = event.url.searchParams.get('keyword')
+  if (keyword) {
+    where.pegawai = {
+      nama: {
+        contains: keyword,
+        mode: 'insensitive'
+      }
+    }
+  }
   const absens = await db.absen.findMany({
-    where: {
-      jadwalId: parseInt(event.params.id)
-    },
+    where,
     include: {
       pegawai: true
     }
--- src/routes/app/jadwal/[id]/member.svelte
+++ src/routes/app/jadwal/[id]/member.svelte
@@ -1,12 +1,16 @@
 <script>
   import { getContext } from 'svelte'
+  import { browser } from '$app/env'
+
   import FButton from '$lib/fbutton.svelte'
   import FInput from '$lib/finput.svelte'
   import { status } from '$lib/format/absen'
+  import { client_fetch_json } from '$lib/http'
 
   const jadwal = getContext('jadwal');
 
   export let members;
+  let keyword = '';
 
   $: formatted = members.map(it => ({
     ...it,
@@ -14,6 +18,22 @@
       workStatus: status(it.workStatus)
     }
   }))
+
+  async function loadMembers(keyword) {
+    if (!browser) {
+      return;
+    }
+    const resp = await client_fetch_json({
+      method: 'GET',
+      path: `/app/jadwal/${jadwal.id}/member`,
+      params: {
+        keyword
+      }
+    })
+    members = resp.members
+  }
+
+  $: loadMembers(keyword);
 </script>
 
 <div class="container px-4 flex flex-col py-6">
@@ -24,6 +44,7 @@
       <input 
         class="rounded px-2 py-1 font-semibold text-gray-700 border-2 disabled:bg-gray-100 disabled:opacity-75"
         placeholder="keyword.." 
+        bind:value={keyword}
       />
       <FButton
         path={`/app/absen/create?jadwal=${jadwal.id}`}
