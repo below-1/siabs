c973b0c09b9364456f6e50d58d858b9cab391cfc
Below Bed <jordanmeta4677@gmail.com>
2022-05-11 00:13:06 +0800

    list pegawai

--- src/routes/app/pegawai/index.svelte
+++ src/routes/app/pegawai/index.svelte
@@ -1,6 +1,8 @@
 <script>
   import {getContext } from 'svelte'
+  import PageHeader from '$lib/page-header.svelte'
   import FButton from '$lib/fbutton.svelte'
+  import FInput from '$lib/finput.svelte'
 
   const cu = getContext('currentUser');
   const user = cu.getUser();
@@ -8,27 +10,33 @@
   export let items = [];
 </script>
 
-<section class='section border-b border-gray-200'>
-  <div class="container py-12 flex flex-col gap-y-4 px-4">
+<PageHeader>
+  <div class="flex flex-col md:flex-row justify-between items-center gap-y-2">
     <h1 class="font-black text-3xl">Daftar Pegawai</h1>
-    <div class="flex flex-wrap gap-x-4">
-      {#if user.superUser}
-      <FButton 
-        size="sm" 
-        path="/app/pegawai/create"
-        outline
-      >
-        tambah pegawai
-      </FButton>
-      {/if}
-    </div>
+    {#if user.superUser}
+    <FButton 
+      size="lg"
+      path="/app/pegawai/create"
+      primary
+    >
+      tambah pegawai
+    </FButton>
+    {/if}
   </div>
-</section>
+</PageHeader>
 
 <section class="container">
-  <ul class="w-full md:w-2/3">
+  <div class="w-full py-6 flex flex-col gap-y-4">
+    <div class="px-4 w-full md:w-1/3">
+      <FInput
+        name="keyword"
+        placeholder="Keyword..."
+      />
+    </div>
     {#each items as item}
-      <li class="px-4 border-b border-gray-200 py-4 flex items-center gap-x-4">
+      <a 
+        href={`/app/pegawai/${item.id}/overview`}
+        class="px-4 border-b border-gray-200 py-4 flex items-center gap-x-4">
         <img
           class="w-12 h-12 rounded"
           src={`https://i.pravatar.cc/150?img=${item.id}`}
@@ -38,12 +46,7 @@
           <div class="text-sm">{item.nip}, {item.nik}</div>
         </div>
         <div class="flex-grow"></div>
-        <a 
-          href={`/app/pegawai/${item.id}/overview`}
-          class="border border-gray-200 rounded bg-gray-50 px-2 py-1 font-semibold text-gray-500">
-          detail
-        </a>
-      </li>
+      </a>
     {/each}
-  </ul>
+  </div>
 </section>
